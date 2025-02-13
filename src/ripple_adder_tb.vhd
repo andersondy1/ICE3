daafaeefcf65fceb0035eddd92924e8b2f03a787
andersondy1 <c27dylan.anderson@afacademy.af.edu>
2025-02-12 21:25:45 -0700

    simulation complete

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,9 +49,16 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
+	   w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with 0 + 0 + Cin" severity failure;
+	   w_addends <= x"22"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"4" and w_Cout = '0') report "bad with 2+2+0" severity failure;
+	   w_addends <= x"57"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"D" and w_Cout = '0') report "bad with 5+7+1" severity failure;
+
+	   
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
-	
+
 end test_bench;
